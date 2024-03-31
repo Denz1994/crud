2567b90bb0a2d8fc663a2f0fca1363c143754013
denz1994 <denz1994@gmail.com>
2024-03-31 01:22:26 -0400

    Add questions, answers, and choices in form. Add restart and boundaries for end of quiz. Add question not found state.

--- waggable/src/app/page.tsx
+++ waggable/src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState } from "react";
 
 interface question {
@@ -26,21 +25,86 @@ export default function Home() {
     correctAnswer: "monkey",
     choices: ["apple", "car", "monkey", "book"],
   };
-
   const questions: question[] = [question1, question2, question3];
+  const initialQuestion = questions[0];
 
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const onSubmitHandler = (event) => {
-    event?.preventDefault();
-    setCurrentQuestion((prev) => {
+  const [showScore, setShowScore] = useState<boolean>(false);
+  const [showErrorPrompt, setShowErrorPrompt] = useState<boolean>(false);
+  const [currentQuestion, setCurrentQuestion] = useState<question | null>(initialQuestion);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(1);
+
+  const updateQuestion = () => {
+    setCurrentQuestionIndex((prev: number) => {
       return prev + 1;
     });
+    setCurrentQuestion(questions[currentQuestionIndex]);
   };
 
+  const updateQuestionDisplay = () => {
+    const hasReachedFinalQuestion = currentQuestionIndex >= questions.length;
+    setShowScore(currentQuestionIndex >= questions.length);
+    setShowErrorPrompt(0 <= currentQuestionIndex && currentQuestionIndex >= questions.length);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    updateQuestion();
+    updateQuestionDisplay();
+  };
+
+  const onResetHandler = (event) => {
+    event.preventDefault();
+    setCurrentQuestion(initialQuestion);
+    setCurrentQuestionIndex(1);
+    setShowScore(false);
+    setShowErrorPrompt(false);
+  };
+
+  let questionHeader = !showErrorPrompt ? <h4>Question {currentQuestionIndex}</h4> : <h4>No Question Found</h4>;
+
+  const restartButton = (
+    <button type="reset" onClick={onResetHandler}>
+      Restart
+    </button>
+  );
+  const questionForm = (
+    <section>
+      <form onReset={(event) => onResetHandler(event)} onSubmit={(event) => onSubmitHandler(event)}>
+        <p>{currentQuestion?.prompt}</p>
+        <fieldset>
+          {currentQuestion?.choices.map((choice, index) => {
+            return (
+              <section>
+                <input type="radio" name="answer" value={choice} />
+                <label htmlFor="answer">{choice}</label>
+              </section>
+            );
+          })}
+        </fieldset>
+        <button type="submit">Submit</button>
+        {restartButton}
+      </form>
+    </section>
+  );
+
+  /**
+   * TODO:
+   * - Record answers
+   * - Implement scoring
+   * - Radio buttons should carry selected value to next question
+   * - Can't click text to select radio button
+   * - Decouple sections into components of their own. Pass props.
+   *
+   *
+   */
+
   return (
-    <form onSubmit={onSubmitHandler}>
-      <p>{currentQuestion}</p>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <section>
+        <h1>Quiz</h1>
+        {questionHeader}
+      </section>
+      {!showScore ? questionForm : restartButton}
+    </>
   );
 }
