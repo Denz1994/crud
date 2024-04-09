cc72ad5e55cc8d83d0a375b9d3afcbe4faa98d31
denz1994 <denz1994@gmail.com>
2024-04-08 23:09:45 -0400

    Add a couple more unit tests. Hardcoded cases. Also update gitignore

--- waggable/src/spec.safetest.tsx
+++ waggable/src/spec.safetest.tsx
@@ -8,4 +8,18 @@ describe("Home", () => {
     await expect(page.locator("text=Quiz")).toBeVisible();
     expect(await page.screenshot()).toMatchImageSnapshot();
   });
+  it("can render a header with a question", async () => {
+    const { page } = await render(<Home />);
+    await expect(page.locator("text=What color is the sky?")).toBeVisible();
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+  it("can render a header with a question and choices", async () => {
+    const { page } = await render(<Home />);
+    await expect(page.locator("text=blue")).toBeVisible();
+    await expect(page.locator("text=green")).toBeVisible();
+    await expect(page.locator("text=red")).toBeVisible();
+    await expect(page.locator("text=orange")).toBeVisible();
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
 });
+``;
